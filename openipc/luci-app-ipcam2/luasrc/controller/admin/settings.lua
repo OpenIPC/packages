ae0775af44cfc7ba5cdb45ed559ce707e6966496
Igor Zalatov <flyrouter@gmail.com>
2021-04-25 19:18:06 +0300

    Add luci-app-ipcam2

--- openipc/luci-app-ipcam2/luasrc/controller/admin/settings.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.admin.settings", package.seeall)
-
-function index()
-	entry({"admin", "ipcam", "settings"}, cbi("admin_ipcam/settings"), _("Settings"))
-end
--- openipc/luci-app-ipcam2/luasrc/controller/admin/snmpd.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.admin.snmpd", package.seeall)
-
-function index()
-	entry({"admin", "ipcam", "mini_snmpd"}, cbi("admin_ipcam/mini_snmpd"), _("Mini SNMPd"))
-end
